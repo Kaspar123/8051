f1b7a35a34140de9e289ad89b48b7075aec165ca
Kaspar123 <kasparhaavajoe@gmail.com>
2018-04-23 19:14:12 +0300

    docs

--- unitronics_parser_c.py
+++ unitronics_parser_c.py
@@ -13,7 +13,39 @@ READ_BUFFER_NAME = 'in_buffer';
 ACTUAL_BYTES_COUNT_VARIABLE = '&actual';
 DEVICE_VARIABLE = 'dev_handle';
 
-
+MAPPING = {
+'0x1E': 'CP210X_SET_BAUDRATE', 
+'0x04': 'CP210X_GET_LINE_CTL', 
+'0x12': 'CP210X_PURGE', 
+'0x19': 'CP210X_SET_CHARS', 
+'0x0A': 'CP210X_SET_XOFF', 
+'0x05': 'CP210X_SET_BREAK', 
+'0x03': 'CP210X_SET_LINE_CTL', 
+'0x06': 'CP210X_IMM_CHAR', 
+'0x0F': 'CP210X_GET_PROPS', 
+'0x15': 'CP210X_EMBED_EVENTS', 
+'0x13': 'CP210X_SET_FLOW', 
+'0x00': 'CP210X_IFC_ENABLE', 
+'0x01': 'CP210X_SET_BAUDDIV', 
+'0x11': 'CP210X_RESET', 
+'0x16': 'CP210X_GET_EVENTSTATE', 
+'0x0C': 'CP210X_GET_EVENTMASK', 
+'0x1D': 'CP210X_GET_BAUDRATE', 
+'0xc0': 'REQTYPE_DEVICE_TO_HOST', 
+'0x40': 'REQTYPE_HOST_TO_DEVICE', 
+'0x07': 'CP210X_SET_MHS', 
+'0x08': 'CP210X_GET_MDMSTS', 
+'0x0B': 'CP210X_SET_EVENTMASK', 
+'0x09': 'CP210X_SET_XON', 
+'0x0E': 'CP210X_GET_CHARS', 
+'0x14': 'CP210X_GET_FLOW', 
+'0x02': 'CP210X_GET_BAUDDIV', 
+'0xc1': 'REQTYPE_INTERFACE_TO_HOST', 
+'0x10': 'CP210X_GET_COMM_STATUS', 
+'0x0D': 'CP210X_SET_CHAR', 
+'0x41': 'REQTYPE_HOST_TO_INTERFACE', 
+'0xFF': 'CP210X_VENDOR_SPECIFIC'
+}
 
 def concatenate(l):
     l = [i[2:] for i in l]
@@ -49,11 +81,11 @@ if __name__ == '__main__':
             endpoint = int(l[21], 0)
             if (transfer_type == CONTROL_TRANSFER):
                 bmRequestType = l[28];
-                bRequest = l[29];
+                bRequest = str(l[29]).upper().replace('X', 'x');
                 wValue = concatenate(l[30:32][::-1]);
                 wIndex = concatenate(l[32:34][::-1]);
                 wLength = l[34];
-                line = 'libusb_control_transfer({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7});'.format(DEVICE_VARIABLE, bmRequestType, bRequest, wValue, wIndex, READ_BUFFER_NAME, wLength, BULK_TIMEOUT);
+                line = 'libusb_control_transfer({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7});'.format(DEVICE_VARIABLE, bmRequestType, MAPPING[bRequest], wValue, wIndex, READ_BUFFER_NAME, wLength, BULK_TIMEOUT);
                 print(line, end='\n');
             elif (transfer_type == BULK_TRANSFER):
                 length = int(l[23], 0)
